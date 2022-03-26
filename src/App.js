d45681287bd6b7b0dec08e4300d3e6847f61c7a5
Farabi Askar <askar.farabi12@gmail.com>
2022-03-26 14:58:14 +0600

    clear version

--- src/App.js
+++ src/App.js
@@ -3,27 +3,6 @@ import './App.css';
 import { CommentBlock } from './components/CommentBlock'
 import React, { useState, useEffect } from 'react'
 
-// const comments = [
-//   {
-//     text: 'Hot Stuff!',
-//     created: '11:00 26.03.2022',
-//     authorName: 'Vasya Pupkin',
-//     authorImage: 'https://cdn.promodj.com/afs/ff76b55b804c8b9cbbed5a68b82609a111%3Aresize%3A440x732%3Asame%3A37cac4'
-//   },
-//   {
-//     text: 'Hot Stuff 2!',
-//     created: '11:00 26.03.2022',
-//     authorName: 'Almas',
-//     authorImage: 'https://cdn.promodj.com/afs/ff76b55b804c8b9cbbed5a68b82609a111%3Aresize%3A440x732%3Asame%3A37cac4'
-//   },
-//   {
-//     text: 'Hot Stuff 3!',
-//     created: '11:00 26.03.2022',
-//     authorName: 'Vasya Pupkin',
-//     authorImage: 'https://cdn.promodj.com/afs/ff76b55b804c8b9cbbed5a68b82609a111%3Aresize%3A440x732%3Asame%3A37cac4'
-//   },
-// ]
-
 function App() {
   const [comments, setComments] = useState([])
 
